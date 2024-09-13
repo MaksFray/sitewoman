d5b56268b0e1b9d9b1651fcf7655a7c328e3d3ce
Maks <fray09@inbox.ru>
2024-09-13 11:06:03 +0600

    Add woman app

--- sitewoman/sitewoman/settings.py
+++ sitewoman/sitewoman/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'woman',
+    'woman.apps.WomanConfig',
 ]
 
 MIDDLEWARE = [
