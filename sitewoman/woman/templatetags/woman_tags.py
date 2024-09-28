f0e92b9ca7cecc269b02c57dc0e454ad7636f211
Maks <fray09@inbox.ru>
2024-09-28 16:36:49 +0600

    Add filter to woman tags to woman app

--- sitewoman/woman/templatetags/woman_tags.py
+++ sitewoman/woman/templatetags/woman_tags.py
@@ -1,14 +1,16 @@
 from django import template
+from django.db.models import Count
+
 import woman.views as views
 from woman.models import Category, Tag
 
 register = template.Library()
 @register.inclusion_tag('woman/list_categories.html')
 def show_categories(category_selected=0):
-    categories = Category.objects.all()
+    categories = Category.objects.annotate(total=Count("posts")).filter(total__gt=0)
     return {'categories': categories, 'category_selected':category_selected}
 
 @register.inclusion_tag('woman/list_tags.html')
 def show_all_tags():
-    tags = Tag.objects.all()
+    tags = Tag.objects.annotate(total=Count("posts")).filter(total__gt=0)
     return {'tags': tags}
