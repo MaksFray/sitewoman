769bd214e62f0ef3022642f66d04573056f3db94
Maks <fray09@inbox.ru>
2024-10-03 13:55:25 +0600

    Add form for adding post to woman app

--- sitewoman/woman/forms.py
+++ sitewoman/woman/forms.py
@@ -3,8 +3,8 @@ from .models import Category, Husband
 
 class AddPostForm(forms.Form):
     title = forms.CharField(max_length=255)
-    slug = forms.SlugField(max_length=255)
+    slug = forms.SlugField(max_length=255, label="URL")
     content = forms.CharField(widget=forms.Textarea(), required=False)
-    is_published = forms.BooleanField(required=False)
-    category = forms.ModelChoiceField(queryset=Category.objects.all())
-    husband = forms.ModelChoiceField(queryset=Husband.objects.all())
+    is_published = forms.BooleanField(required=False, label="Status", initial=True)
+    category = forms.ModelChoiceField(queryset=Category.objects.all(), empty_label="No category")
+    husband = forms.ModelChoiceField(queryset=Husband.objects.all(), empty_label="Not married")
