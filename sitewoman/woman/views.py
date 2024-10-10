24791e2df18282d85504198a6175e936802b89d0
Maks <fray09@inbox.ru>
2024-10-10 22:31:42 +0600

    Add upload file view to woman app

--- sitewoman/woman/views.py
+++ sitewoman/woman/views.py
@@ -23,8 +23,13 @@ def index(request):
     }
     return render(request, 'woman/index.html', context=data)
 
-
+def handle_uploaded_file(f):
+    with open(f"uploads/{f.name}", "wb+") as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
 def about(request):
+    if request.method == 'POST':
+        handle_uploaded_file(request.FILES['file_upload'])
     return render(request, 'woman/about.html')
 
 
@@ -39,7 +44,7 @@ def add_page(request):
     data = {
         'title': 'Add post',
         'menu': MENU,
-        #'posts': posts,
+        # 'posts': posts,
         'category_selected': 0,
         'form': form,
     }
