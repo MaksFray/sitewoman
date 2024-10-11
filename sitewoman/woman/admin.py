adb9d37edf1f78b4f3118e3749806b2647a5e907
Maks <fray09@inbox.ru>
2024-10-11 17:01:13 +0600

    Add uploaded file to admin panel to woman app

--- sitewoman/woman/admin.py
+++ sitewoman/woman/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin, messages
+from django.utils.safestring import mark_safe
+
 from .models import Woman, Category
+
 
 class MarriageFilter(admin.SimpleListFilter):
     title = "Marriage status"
@@ -10,28 +13,39 @@ class MarriageFilter(admin.SimpleListFilter):
             ('married', 'Married'),
             ('single', 'Single')
         ]
+
     def queryset(self, request, queryset):
         if self.value() == 'married':
             return queryset.filter(husband__isnull=False)
         elif self.value() == 'single':
             return queryset.filter(husband__isnull=True)
 
+
 @admin.register(Woman)
 class WomanAdmin(admin.ModelAdmin):
-    fields = ['title', 'slug', 'photo', 'content', 'category', 'husband', 'tags']
-    prepopulated_fields = {'slug':('title',),}
+    fields = ['title', 'slug', 'photo', 'post_photo','content', 'category', 'husband', 'tags']
+    prepopulated_fields = {'slug': ('title',), }
+    readonly_fields = ['post_photo']
     filter_horizontal = ['tags']
-    list_display = ('title', 'time_create', 'is_published', 'category', 'brief_info')
-    list_display_links = ('title', )
+    list_display = ('title', 'time_create', 'is_published', 'category', 'post_photo')
+    list_display_links = ('title',)
     ordering = ('time_create', 'title')
     list_editable = ('is_published', 'category')
     list_per_page = 10
     actions = ['set_published', 'set_unpublished']
     search_fields = ['title', 'category__name']
     list_filter = [MarriageFilter, 'category__name', 'is_published']
+
     @admin.display(description="Short description", ordering="content")
     def brief_info(self, woman: Woman):
         return f"Description {len(woman.content)} symbols"
+
+    @admin.display(description="Photo", ordering="content")
+    def post_photo(self, woman: Woman):
+        if woman.photo:
+            return mark_safe(f"<img src='{woman.photo.url}' width=50>")
+        else:
+            return "No photo"
 
     def set_published(self, request, queryset):
         count = queryset.update(is_published=Woman.Status.PUBLISHED)
@@ -40,6 +54,7 @@ class WomanAdmin(admin.ModelAdmin):
     def set_unpublished(self, request, queryset):
         count = queryset.update(is_published=Woman.Status.DRAFT)
         self.message_user(request, f"{count} posts were updated", messages.WARNING)
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
